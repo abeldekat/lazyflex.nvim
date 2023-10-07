66606b47661b0d473aa49e47287fbf3f376904bd
abeldekat <eendebeer@tweeberen.xyz>
2023-10-07 17:31:37 +0200

    feat: ci, added e2e to test lazyflex.nvim and lazy.nvim integration.

--- tests/core/e2e_spec.lua
+++ tests/core/e2e_spec.lua
@@ -1,13 +1,40 @@
 local assert = require("luassert")
 
-describe("my first test", function()
-  local tests = {
-    { name = "test" },
-    { name = "test" },
-  }
-  for _, test in ipairs(tests) do
-    it("does something", function()
-      assert.equal(test.name, "test")
-    end)
-  end
+local function get_dir()
+  local f = debug.getinfo(1, "S").source:sub(2)
+  return vim.fn.fnamemodify(f, ":p:h:h:h")
+end
+
+describe("lazyflex.nvim", function()
+  before_each(function()
+    vim.g.lazy_did_setup = false
+    vim.go.loadplugins = true
+    for modname in pairs(package.loaded) do
+      if modname:find("lazy") == 1 then
+        package.loaded[modname] = nil
+      end
+    end
+  end)
+
+  it("integrates with lazy.nvim", function()
+    local Lazy = require("lazy")
+    local Config = require("lazy.core.config")
+
+    Lazy.setup({
+      {
+        "abeldekat/lazyflex.nvim",
+        dir = get_dir(),
+        import = "lazyflex.plugins.intercept",
+        opts = { keywords = { "paint" } },
+      },
+      "folke/neodev.nvim",
+      "folke/paint.nvim",
+    }, { install_missing = true })
+
+    assert(3 == vim.tbl_count(Config.plugins))
+
+    local disabled = Config.spec.disabled["neodev.nvim"]
+    assert.is_table(disabled)
+    assert.is_false(disabled._.cond)
+  end)
 end)
--- tests/init.lua
+++ tests/init.lua
@@ -5,6 +5,20 @@ function M.root(root)
   return vim.fn.fnamemodify(f, ":p:h:h") .. "/" .. (root or "")
 end
 
+function M.bootstrap_lazy(lazypath)
+  if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system({
+      "git",
+      "clone",
+      "--filter=blob:none",
+      "https://github.com/folke/lazy.nvim.git",
+      "--branch=stable",
+      lazypath,
+    })
+  end
+  vim.opt.rtp:prepend(lazypath)
+end
+
 ---@param plugin string
 function M.load(plugin)
   local name = plugin:match(".*/(.*)")
@@ -27,11 +41,12 @@ function M.setup()
   vim.opt.runtimepath:append(M.root())
   vim.opt.packpath = { M.root(".tests/site") }
   M.load("nvim-lua/plenary.nvim")
-  M.load("folke/lazy.nvim")
+
   vim.env.XDG_CONFIG_HOME = M.root(".tests/config")
   vim.env.XDG_DATA_HOME = M.root(".tests/data")
   vim.env.XDG_STATE_HOME = M.root(".tests/state")
   vim.env.XDG_CACHE_HOME = M.root(".tests/cache")
+  M.bootstrap_lazy(M.root(".tests/data/nvim/lazy/lazy.nvim"))
 end
 
 M.setup()

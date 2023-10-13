33355dbfbfb18bb82d59c5e686f6cef9972db173
Iordanis Petkakis <dpetka2001@gmail.com>
2023-10-13 22:11:09 +0300

    refactor code into function

--- lua/lazyflex/attach.lua
+++ lua/lazyflex/attach.lua
@@ -19,6 +19,17 @@ local function should_enable(name, keywords, enable_match)
   return not enable_match
 end
 
+-- invert the value of enable_match for plugins that match keywords in `kw_invert`
+local function should_invert(name, keywords)
+  name = string.lower(name)
+  for _, keyword in ipairs(keywords) do
+    if name:find(keyword, 1, true) then
+      return true
+    end
+  end
+  return false
+end
+
 local function is_optional_in_core(plugin)
   -- when a plugin is removed from core,
   -- plugin.optional and plugin.enabled are used to inform the user
@@ -61,11 +72,9 @@ function M.attach(opts, adapter)
     -- plugin is enabled:
     plugin.cond = should_enable(name, opts.kw, opts.enable_match)
     if opts.kw_invert and not vim.tbl_isempty(opts.kw_invert) then
-      for _, keyword in ipairs(opts.kw_invert) do
-        name = string.lower(name)
-        if name:find(keyword, 1, true) then
-          plugin.cond = not opts.enable_match
-        end
+      local invert = should_invert(name, opts.kw_invert)
+      if invert then
+        plugin.cond = not opts.enable_match
       end
     end
 
